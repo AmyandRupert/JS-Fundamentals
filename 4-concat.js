3c0705fca6697532def8a9766c2c90934319a366
AmyandRupert <amyleroux432@gmail.com>
2025-07-29 18:14:42 +0200

    4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,5 +1,3 @@
 const argument= process.argv.slice(2); // remove first 2 default items
-function printArgumentsInFormat(arg1, arg2) {
 
-console.log(`${arg1} is ${arg2}`);
-}
+console.log(`${argument[0]} is ${argument[1]}`);
